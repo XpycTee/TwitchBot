8ee29f27deddeb753ea0e37373d7bd359e6e4f1b
XpycTee <shift1998@bk.ru>
2020-07-15 16:13:18 +0500

    Added module chatCommander for adding custom commands

---
+++ modules/chatComander.py
@@ -0,0 +1,66 @@
+import Utils, Data, yaml, os
+def starter():
+	folderPath = Utils.Bot.moduleFolder(__name__)
+	if not 'commands.yml' in os.listdir(folderPath):
+		nulDict = {}
+		with open(f'{folderPath}\\commands.yml', 'w') as commands:
+			commandsData = yaml.dump(nulDict, commands)
+	with open(f'{folderPath}\\commands.yml') as commands:
+		commandsData = yaml.load(commands, Loader=yaml.FullLoader)
+	Data.Mods.globData["chatCommander"] = commandsData
+
+def responder(message, username):
+	def addCommand(command, respond):
+		Data.Mods.globData["chatCommander"].update({command : respond})
+		folderPath = Utils.Bot.moduleFolder(__name__)
+		with open(f'{folderPath}\\commands.yml', 'w') as commands:
+			caommandsData = yaml.dump(Data.Mods.globData["chatCommander"], commands)
+		return f"{username}, команда !{command} добавлена"
+	def removeCommand(command):
+		try:
+			Data.Mods.globData["chatCommander"].pop(command)
+		except KeyError:
+			return f"{username}, команды !{command} нет"
+		folderPath = Utils.Bot.moduleFolder(__name__)
+		with open(f'{folderPath}\\commands.yml', 'w') as commands:
+			caommandsData = yaml.dump(Data.Mods.globData["chatCommander"], commands)
+		return f"{username}, команда !{command} удалена"
+
+	splitedMsg = message.strip().split(" ")
+	if Utils.Chat.Users.isOp(username):
+		if (splitedMsg[0].lower() == "!rmcommand" or splitedMsg[0].lower() == "!addcommand"):
+			try:
+				command = splitedMsg[1].lower()
+			except IndexError:
+				return f"{username}, команда не введена"
+			if (splitedMsg[0] == "!rmcommand"):
+				return removeCommand(command)	
+			try:
+				respond = splitedMsg[2]
+			except IndexError:
+				return f"{username}, сообщение не введено"
+			if len(splitedMsg) > 3:
+				for wordIndex in range(3,len(splitedMsg)):
+					respond = f"{respond} {splitedMsg[wordIndex]}"
+			if (splitedMsg[0].lower() == "!addcommand"):
+				return addCommand(splitedMsg[1].lower(),respond);
+		
+	commandForRespond = splitedMsg[0].replace("!","")
+	if commandForRespond in Data.Mods.globData["chatCommander"]:
+		if len(splitedMsg) == 1:
+			ret = Data.Mods.globData["chatCommander"][commandForRespond]
+			message = ret.replace('{username}', username)
+			return message
+		else:
+			toWhom = splitedMsg[1].replace("@", "")
+			if toWhom.lower() in Data.Chat.userlist:
+				ret = Data.Mods.globData["chatCommander"][commandForRespond]
+				message = ret.replace('{username}', Data.Chat.userlist[toWhom]['display_name'])
+				return message
+			else:
+				ret = Data.Mods.globData["chatCommander"][commandForRespond]
+				message = ret.replace('{username}', username)
+				return message
+		
+
+		
