789e2c53a086465c850125e63cf8a66451a7471c
XpycTee <shift1998@bk.ru>
2020-07-20 20:01:00 +0500

    Reformated installer

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 echo "create a test service ..."
+useradd -c "User for Bot" -r twitch_bot
 sed 's@path_to_bot@'$PWD'@' twitch_bot.sh > /etc/init.d/twitch_bot
 sed 's@path_to_bot@'$PWD'@' install.service > /etc/systemd/system/twitch_bot.service
+chown -R twitch_bot $PWD
+chgrp -R twitch_bot $PWD
+chmod -R ugo+rw $PWD
 chmod +x /etc/init.d/twitch_bot
-# sed -i "s/Your_User_Name/you_path/g" /etc/init.d/test
 echo "created the test service"
