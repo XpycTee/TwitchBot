eb9dc9b6ca18058c12fa246a8b7d14006440d4bc
XpycTee <shift1998@bk.ru>
2020-07-20 14:16:39 +0500

    Fix unix daemon script

--- twitch_bot.sh
+++ twitch_bot.sh
@@ -12,22 +12,27 @@ DAEMON="/usr/bin/python3 path_to_bot/bot.py"
 case "$1" in
   start)
         echo -n "Starting daemon: "$NAME
-    start-stop-daemon --start --quiet --background --chdir path_to_bot --pidfile $PIDFILE --exec $DAEMON -- $DAEMON_OPTS
+    start-stop-daemon --start --quiet --background --chdir path_to_bot --make-pidfile --pidfile $PIDFILE --exec $DAEMON -- $DAEMON_OPTS
         echo "."
     ;;
   stop)
         echo -n "Stopping daemon: "$NAME
-    start-stop-daemon --stop --quiet --oknodo --pidfile $PIDFILE
+    start-stop-daemon --stop --quiet --oknodo --remove-pidfile --pidfile $PIDFILE
         echo "."
     ;;
   restart)
         echo -n "Restarting daemon: "$NAME
-    start-stop-daemon --stop --quiet --oknodo --retry 30 --pidfile $PIDFILE
-    start-stop-daemon --start --quiet --background --chdir path_to_bot  --pidfile $PIDFILE --exec $DAEMON -- $DAEMON_OPTS
+    start-stop-daemon --stop --quiet --oknodo --retry 30 --remove-pidfile --pidfile $PIDFILE
+    start-stop-daemon --start --quiet --background --chdir path_to_bot --make-pidfile --pidfile $PIDFILE --exec $DAEMON -- $DAEMON_OPTS
+    echo "."
+    ;;
+  status)
+        echo -n "Status daemon: "$NAME
+		echo -n `start-stop-daemon --status --pidfile $PIDFILE`
     echo "."
     ;;
   *)
-    echo "Usage: "$1" {start|stop|restart}"
+    echo "Usage: "$1" {start|stop|restart|status}"
     exit 1
 esac
 
