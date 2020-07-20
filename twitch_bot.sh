0f04b33265a6dde698bbe155ab5e70757673c495
XpycTee <shift1998@bk.ru>
2020-07-20 20:50:20 +0500

    Again wyccS

--- twitch_bot.sh
+++ twitch_bot.sh
@@ -12,7 +12,7 @@ DAEMON="/usr/bin/python3 path_to_bot/bot.py"
 case "$1" in
   start)
         echo -n "Starting daemon: "$NAME
-    start-stop-daemon --start --quiet --background --user twitch_bot --group twitch_bot --chdir path_to_bot --make-pidfile --pidfile $PIDFILE --exec $DAEMON -- $DAEMON_OPTS
+    start-stop-daemon --start --quiet --background --chuid twitch_bot --group twitch_bot --chdir path_to_bot --make-pidfile --pidfile $PIDFILE --exec $DAEMON -- $DAEMON_OPTS
         echo "."
     ;;
   stop)
@@ -23,7 +23,7 @@ case "$1" in
   restart)
         echo -n "Restarting daemon: "$NAME
     start-stop-daemon --stop --quiet --oknodo --retry 30 --remove-pidfile --pidfile $PIDFILE
-    start-stop-daemon --start --quiet --background --user twitch_bot --group twitch_bot --chdir path_to_bot --make-pidfile --pidfile $PIDFILE --exec $DAEMON -- $DAEMON_OPTS
+    start-stop-daemon --start --quiet --background --chuid twitch_bot --group twitch_bot --chdir path_to_bot --make-pidfile --pidfile $PIDFILE --exec $DAEMON -- $DAEMON_OPTS
     echo "."
     ;;
 esac
