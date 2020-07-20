8cf38e21ddc65eaa9788131ff75f4a5d33ebf1c8
XpycTee <shift1998@bk.ru>
2020-07-20 20:30:31 +0500

    Yea-yea again

--- twitch_bot.sh
+++ twitch_bot.sh
@@ -12,18 +12,18 @@ DAEMON="/usr/bin/python3 path_to_bot/bot.py"
 case "$1" in
   start)
         echo -n "Starting daemon: "$NAME
-    start-stop-daemon --start --quiet --background --user twitch_bot --chdir path_to_bot --pidfile $PIDFILE --exec $DAEMON -- $DAEMON_OPTS
+    start-stop-daemon --start --quiet --background --user twitch_bot --group twitch_bot --chdir path_to_bot --make-pidfile --pidfile $PIDFILE --exec $DAEMON -- $DAEMON_OPTS
         echo "."
     ;;
   stop)
         echo -n "Stopping daemon: "$NAME
-    start-stop-daemon --stop --quiet --oknodo --pidfile $PIDFILE
+    start-stop-daemon --stop --quiet --oknodo --remove-pidfile --pidfile $PIDFILE
         echo "."
     ;;
   restart)
         echo -n "Restarting daemon: "$NAME
-    start-stop-daemon --stop --quiet --oknodo --retry 30 --pidfile $PIDFILE
-    start-stop-daemon --start --quiet --background --user twitch_bot --chdir path_to_bot --pidfile $PIDFILE --exec $DAEMON -- $DAEMON_OPTS
+    start-stop-daemon --stop --quiet --oknodo --retry 30 --remove-pidfile --pidfile $PIDFILE
+    start-stop-daemon --start --quiet --background --user twitch_bot --group twitch_bot --chdir path_to_bot --make-pidfile --pidfile $PIDFILE --exec $DAEMON -- $DAEMON_OPTS
     echo "."
     ;;
 esac
